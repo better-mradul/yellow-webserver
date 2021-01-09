7b0a994aed58fa4c9b67853c5e7230738f6e68af
Mradul Jain <mrjain@better.com>
2021-01-08 19:52:49 -0500

    Initial commit

---
+++ src/main/java/com/better/yellowwebserver/YellowWebserverApplication.java
@@ -0,0 +1,13 @@
+package com.better.yellowwebserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YellowWebserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YellowWebserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/better/yellowwebserver/YellowWebserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.better.yellowwebserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YellowWebserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
